9c8a15eb0d750d79509600b8f1b8415669469283
mrbadala <68920754+mrbadala@users.noreply.github.com>
2020-07-30 09:23:47 +0700

    FIX
    Work

--- ubah-huruf.php
+++ ubah-huruf.php
@@ -1,13 +1,20 @@
 <?php
 function ubah_huruf($string){
 //kode di sini
+    $abjad="abcdefghijklmnopqrstuvwxyz";
+    $output="";
+    for ($i=0; $i < strlen($string); $i++) { 
+        $posisi= strrpos($abjad, $string[$i]);
+        $output .=substr($abjad, $posisi+1, 1);
+    }
+    return $output;
 }
 
 // TEST CASES
-echo ubah_huruf('wow'); // xpx
-echo ubah_huruf('developer'); // efwfmpqfs
-echo ubah_huruf('laravel'); // mbsbwfm
-echo ubah_huruf('keren'); // lfsfo
-echo ubah_huruf('semangat'); // tfnbohbu
+echo"<br>".ubah_huruf('wow'); // xpx
+echo"<br>". ubah_huruf('developer'); // efwfmpqfs
+echo"<br>". ubah_huruf('laravel'); // mbsbwfm
+echo"<br>". ubah_huruf('keren'); // lfsfo
+echo"<br>". ubah_huruf('semangat'); // tfnbohbu
 
 ?>
