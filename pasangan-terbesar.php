2165db0631183c3f89d08a1c03f24c861fc41e42
mrbadala <68920754+mrbadala@users.noreply.github.com>
2020-07-30 09:09:38 +0700

    FIX
    Work

--- pasangan-terbesar.php
+++ pasangan-terbesar.php
@@ -1,13 +1,17 @@
 <?php
 function pasangan_terbesar($angka){
 // kode di sini
+    for ($i=0; $i < strlen($angka)-2; $i++) { 
+        echo substr($angka, $i, 2);
+    }
+    return strlen($angka);
 }
 
 // TEST CASES
-echo pasangan_terbesar(641573); // 73
-echo pasangan_terbesar(12783456); // 83
-echo pasangan_terbesar(910233); // 91
-echo pasangan_terbesar(71856421); // 85
-echo pasangan_terbesar(79918293); // 99
+echo pasangan_terbesar(641573). "<br>" ; // 73
+echo pasangan_terbesar(12783456). "<br>"; // 83
+echo pasangan_terbesar(910233). "<br>" ; // 91
+echo pasangan_terbesar(71856421). "<br>"; // 85
+echo pasangan_terbesar(79918293). "<br>"; // 99
 
 ?>
