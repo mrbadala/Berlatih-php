2536af5a001aa9bdc1525d7de98e79c3a28d4dc4
mrbadala <68920754+mrbadala@users.noreply.github.com>
2020-07-30 09:00:03 +0700

    FIX
    Work

--- tentukan-deret-geometri.php
+++ tentukan-deret-geometri.php
@@ -1,6 +1,18 @@
 <?php
 function tentukan_deret_geometri($arr) {
 // kode di sini
+    if (sizeof($arr)<=1) {
+        return true;
+    }
+
+    $ratio = $arr[1]/$arr[0];
+
+    for ($i=1; $i < sizeof($arr); $i++) { 
+        if (($arr[$i]/($arr[$i-1]))!= $ratio) {
+            return "false<br>";
+        }
+    }
+    return "true<br>";
 }
 //TEST CASES
 echo tentukan_deret_geometri([1, 3, 9, 27, 81]); // true
